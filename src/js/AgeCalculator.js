d072a650c06d27933357bcb8b9cedbeaefa85db0
Mike Donovan <mike@mikedonovan.dev>
2023-02-03 15:09:19 -0800

    Revise getYearsSinceEarthBirthday to pass Earth test

--- src/js/AgeCalculator.js
+++ src/js/AgeCalculator.js
@@ -1,6 +1,7 @@
 export default class AgeCalculator {
   constructor() {
     this.planetAgeRatios = {
+      'earth': 1,
       'mercury': 0.24,
       'venus': 0.62,
       'mars': 1.88,
@@ -15,7 +16,7 @@ export default class AgeCalculator {
   getYearsSinceEarthBirthday(options) {
     let output = {};
     for (const planet in this.planetAgeRatios) {
-      output[planet] = 0;
+      output[planet] = options.currentEarthAge - options.pastAgeTarget;
     }
     return output;
   }
